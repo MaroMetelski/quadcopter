dc164e3cd818c0a0417d0629853cee3824db861b
Marek Metelski <marek@metelski.dev>
2023-11-10 21:00:34 +0100

    Use config API to load and apply configuration at startup
    Use config API in main to load and apply configuration
    to subsystems.

--- controller/app/src/main.c
+++ controller/app/src/main.c
@@ -1,6 +1,27 @@
+#include <controller/configs.h>
+#include <controller/config_dispatcher.h>
+#include <app/logging.h>
 #include <zephyr/kernel.h>
 
-void main(void)
+APP_LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
+
+int main(void)
 {
-    printk("Hello world!\n");
+    if (!configs_load_all()) {
+        LOG_ERR("Failed to load configuration from memory!");
+        return -1;
+    }
+    LOG_INF("Loaded configuration from memory");
+
+    if (!config_dispatcher_update_all()) {
+        LOG_ERR("Failed to apply configurations!");
+        return -1;
+    }
+    LOG_INF("Configuration applied to subsystems");
+
+    while (1) {
+        k_sleep(K_MSEC(1000));
+    }
+
+    return 0;
 }
